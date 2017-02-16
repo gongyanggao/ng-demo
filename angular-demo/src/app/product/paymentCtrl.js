c2f87977e074aa44697ad3a04e10f80b75dd1788
gongyanggao <1466563549@qq.com>
2017-02-16 18:14:05 +0800

    asdf

---
+++ angular-demo/src/app/product/paymentCtrl.js
@@ -0,0 +1,9 @@
+'use strict';
+var myAngularDemo;
+myAngularDemo.AppModule.controller('paymentCtrl', ['$scope', function($scope) {
+	layui.use('form', function() {
+		var form = layui.form();
+
+		//各种基于事件的操作，下面会有进一步介绍
+	});
+}]);
