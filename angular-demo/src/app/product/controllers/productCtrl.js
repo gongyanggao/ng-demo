cdaa1722ae09fdcce2fe8f1665444dab37b120b2
gongyanggao <1466563549@qq.com>
2017-02-17 12:03:27 +0800

    1111

--- angular-demo/src/app/product/controllers/productCtrl.js
+++ angular-demo/src/app/product/controllers/productCtrl.js
@@ -1,30 +1,26 @@
 'use strict';
 var myAngularDemo;
-myAngularDemo.AppModule.controller('productCtrl', ['$scope', 'instance', '$http', function($scope, instance, $http) {
-	$scope.minus = function(item) {
-		instance.countMinus(item);
-	};
-	$scope.plus = function(item) {
-		instance.countPlus(item);
-	};
-	$scope.payList = instance.getPayList();
-	function getProducts() {
-		$http({
-			method: 'GET',
-			url: '../data/products.json'
-		}).success(function(data) {
-			$scope.datas = data.data;
+myAngularDemo.AppModule.controller('productCtrl', ['$scope', 'instance', 'ajaxService',
+	function($scope, instance, ajaxService) {
+		$scope.minus = function(item) {
+			instance.countMinus(item);
+		};
+		$scope.plus = function(item) {
+			instance.countPlus(item);
+		};
+		$scope.payList = instance.getPayList();
+		ajaxService.get('../data/products.json', function(data) {
+			$scope.datas = data;
 			if (instance.getPayList().length > 0) {
 				for (var i = $scope.datas.length - 1; i >= 0; i--) {
 					for (var j = instance.getPayList().length - 1; j >= 0; j--) {
-						if(instance.getPayList()[j].productId === $scope.datas[i].productId) {
+						if (instance.getPayList()[j].productId === $scope.datas[i].productId) {
 							$scope.datas[i].count = instance.getPayList()[j].count;
 							break;
 						}
 					}
 				}
 			}
-		}).error(function() {});
+		});
 	}
-	getProducts();
-}]);
+]);
