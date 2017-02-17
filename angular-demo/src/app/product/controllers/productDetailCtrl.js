76e3e15997bfc6e2c116d0b6fe028042ffddaabd
gongyanggao <1466563549@qq.com>
2017-02-17 16:01:54 +0800

    112

--- angular-demo/src/app/product/controllers/productDetailCtrl.js
+++ angular-demo/src/app/product/controllers/productDetailCtrl.js
@@ -1,9 +1,9 @@
 'use strict';
-var myAngularDemo;
-myAngularDemo.AppModule.controller('productDetailCtrl', ['$scope', '$stateParams', 'ajaxService',
+var myNgDemo;
+myNgDemo.CtrlModule.controller('productDetailCtrl', ['$scope', '$stateParams', 'ajaxService',
 	function($scope, $stateParams, ajaxService) {
 		ajaxService.get('../data/productsDetails.json', function(data) {
-			$scope.introduction = data[$stateParams.productId - 1].words;
+			$scope.introduction = data.data[$stateParams.productId - 1].words;
 		});
 
 	}
