755a071707ad0673e98823248e2455cef7e7332e
gongyanggao <1466563549@qq.com>
2017-02-17 12:03:50 +0800

    11111

--- angular-demo/src/app/product/controllers/productDetailCtrl.js
+++ angular-demo/src/app/product/controllers/productDetailCtrl.js
@@ -1,17 +1,10 @@
 'use strict';
 var myAngularDemo;
-myAngularDemo.AppModule.controller('productDetailCtrl', ['$scope', '$stateParams', '$http', function($scope, $stateParams, $http) {
-	var index = $stateParams.productId;
-	function getProductDetails() {
-		// body...
-		$http({
-			method: 'GET',
-			url: '../data/productsDetails.json'
-		}).success(function(data) {
-			$scope.introduction = data[index-1].words;
-			$scope.$digest();
-		}).error(function() {});
-	}
-	getProductDetails();
+myAngularDemo.AppModule.controller('productDetailCtrl', ['$scope', '$stateParams', 'ajaxService',
+	function($scope, $stateParams, ajaxService) {
+		ajaxService.get('../data/productsDetails.json', function(data) {
+			$scope.introduction = data[$stateParams.productId - 1].words;
+		});
 
-}]);
+	}
+]);
