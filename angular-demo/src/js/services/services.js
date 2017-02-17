210d99f8a8ff5e0e3f8f444a1a1fae2a9a4fd09f
gongyanggao <1466563549@qq.com>
2017-02-17 12:02:56 +0800

    !@

--- angular-demo/src/js/services/services.js
+++ angular-demo/src/js/services/services.js
@@ -4,6 +4,7 @@ myAngularDemo.AppModule
         var payList = [];
         return {
             countMinus: function(item) {
+                //数量大于1则减1
                 if (item.count > 1) {
                     for (var i = 0; i < payList.length; i++) {
                         if (payList[i].productId === item.productId) {
@@ -12,6 +13,7 @@ myAngularDemo.AppModule
                             break;
                         }
                     }
+                    //等于1的时候减1再清空购物车中对应的条目
                 } else if (item.count > 0) {
                     for (var i = 0; i < payList.length; i++) {
                         if (payList[i].productId === item.productId) {
@@ -22,10 +24,12 @@ myAngularDemo.AppModule
                     item.count--;
                 }
             },
+            //等于0的时候，数量加1，购物车添加条目
             countPlus: function(item) {
                 if (item.count == 0) {
                     item.count++;
                     payList.push(item);
+                    //大于0的时候，数量加1，购物车中对应条目+1
                 } else {
                     for (var i = 0; i < payList.length; i++) {
                         if (payList[i].productId === item.productId) {
@@ -37,6 +41,7 @@ myAngularDemo.AppModule
                     // item.count++;
                 }
             },
+            //移除购物车中的条目
             removeItem: function(id) {
                 for (var i = 0; i < payList.length; i++) {
                     if (payList[i].productId === id) {
@@ -51,39 +56,15 @@ myAngularDemo.AppModule
         };
     })
     /*
-     * Ajax请求后面需要拼接时间戳，否则后台接口报错
+     * Ajax服务
      */
     .service('ajaxService', ['$http', function($http) {
         var _self = this;
 
-        function _ajax(option) {
-            option.params && (option.params['_'] = new Date().getTime());
-            return $http(option);
-        }
+        _self.get = function(url, success) {
+            $http.get(url).success(function(data) {
+                success(data.data);
+            })
+        };
 
-        /**
-         * 设置时间戳
-         * @param data
-         */
-        function setTimestamp(data) {
-            data || (data = {
-                params: {}
-            });
-            data.params || (data.params = {});
-            data.params['_'] = new Date().getTime();
-        }
-
-        function _get(url, data) {
-            setTimestamp(data);
-            return $http.get(url, data);
-        }
-
-        function _post(url, data) {
-            setTimestamp
-            return $http.post(url, data);
-        }
-
-        _self.ajax = _ajax;
-        _self.get = _get;
-        _self.post = _post;
-    }])
+    }]);
