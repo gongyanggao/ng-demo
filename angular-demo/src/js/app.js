dab7043f03f36b0d65fa0232240dc84de311779f
gongyanggao <1466563549@qq.com>
2017-04-07 09:33:29 +0800

    Update app.js

--- angular-demo/src/js/app.js
+++ angular-demo/src/js/app.js
@@ -3,9 +3,9 @@ var myNgDemo;
 	'use strict';
 	myNgDemo.CtrlModule = angular.module("myNgDemo.controllers", []),
 		myNgDemo.ServiceModule = angular.module("myNgDemo.services", []),
-		// myNgDemo.DirectiveModule = angular.module("myNgDemo.directives", []),
+		myNgDemo.DirectiveModule = angular.module("myNgDemo.directives", []),
 		// myNgDemo.TemplateModule = angular.module("templates", ["ng"]),
-		myNgDemo.AppModule = angular.module("myNgDemo", ["myNgDemo.controllers", "myNgDemo.services", "ui.router"]);
+		myNgDemo.AppModule = angular.module("myNgDemo", ["myNgDemo.controllers", "myNgDemo.services", "myNgDemo.directives", "ui.router"]);
 	myNgDemo.AppModule.controller('angularDemoAppCtrl', function($scope, $rootScope) {
 		// TODO
 
