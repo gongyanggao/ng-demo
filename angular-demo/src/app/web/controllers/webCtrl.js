97431e2e8576eaa24c023885c1b34f29f2e011bf
gongyanggao <1466563549@qq.com>
2017-04-07 09:29:14 +0800

    Update webCtrl.js

--- angular-demo/src/app/web/controllers/webCtrl.js
+++ angular-demo/src/app/web/controllers/webCtrl.js
@@ -2,4 +2,10 @@ var myNgDemo;
 myNgDemo.CtrlModule.controller('webCtrl', ['$scope', function ($scope) {
         'use strict';
         //TODO
+        $.ajax({
+        	url: 'app/web/views/replace.html',
+        	success: function(str) {
+        		$('#replace').html(str);
+        	}
+        })
     }]);
