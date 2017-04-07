f35b65a935251aec1628295d0f3bd2598cfe641a
gongyanggao <1466563549@qq.com>
2017-04-07 09:26:15 +0800

    Update bigDataCtrl.js

--- angular-demo/src/app/bigdata/controllers/bigDataCtrl.js
+++ angular-demo/src/app/bigdata/controllers/bigDataCtrl.js
@@ -1,5 +1,16 @@
 var myNgDemo;
-myNgDemo.CtrlModule.controller('bigDataCtrl', ['$scope', function ($scope) {
-        'use strict';
-        //TODO
-    }]);
+myNgDemo.CtrlModule.controller('bigDataCtrl', ['$scope', function($scope) {
+	'use strict';
+	//TODO
+	$('#domainTable').bootstrapTable({
+		url: './app/bigdata/controllers/data.json',
+		method: 'get',
+		dataType: 'json',
+		pagination: true,
+		sortable: true,
+		pageNumber: 1,
+		pageSize: 5,
+		pageList: [5, 10, 20, 50],
+		height: 400
+	});
+}]);
